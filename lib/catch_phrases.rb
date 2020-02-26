b832967609c045017cd2422f10110a7bef9ca467
Luis Pestana <lpestana@protonmail.com>
2020-02-26 16:08:09 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,4 +1,3 @@
-status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 
 def mario
   phrase = "It's-a me, Mario!" 
@@ -7,5 +6,6 @@ def mario
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
